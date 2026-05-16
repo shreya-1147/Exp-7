0e5a5d85f543ea39cbda01ef5a3701e2e2c6f46c
Shreya Rastogi <shreya9236094193@gmail.com>
2026-05-16 14:53:04 +0530

    c3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function Student({ name, course, marks }) {
   return (
     <div className="student-card">
       <h2>{name}</h2>
-      <p>Course: {course}</p>
+      <p>Course:{course}</p>
       <p>Marks:{marks}</p>
     </div>
   )
